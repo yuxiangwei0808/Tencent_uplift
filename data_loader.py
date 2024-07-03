b0c1132f1cca8c09d043c8866d669b6cfd622bdc
yuxiangwei0808 <weiyuxiang0808@gmail.com>
2024-07-03 17:18:44 +0800

    update data_loader from dev

--- data_loader.py
+++ data_loader.py
@@ -5,10 +5,9 @@ import gzip
 import h5py
 import pandas as pd
 import numpy as np
-from torch.utils.data import DataLoader, Dataset, RandomSampler, DistributedSampler
+from torch.utils.data import DataLoader, Dataset, RandomSampler, DistributedSampler, Subset
 import torch
-from sklearn.model_selection import KFold
-from datasets import load_dataset
+from sklearn.model_selection import KFold, StratifiedKFold
 import threading
 from multiprocessing import Process, Queue, current_process
 
@@ -18,6 +17,54 @@ def load_one_gz_file(file_path, field):
                                 na_values='NULL')
     df = df.sample(frac=1).reset_index(drop=True)  # shuffle the data
     return df
+
+
+def load_df(source_dir, field, is_train, num_folds, fold_id):
+        # in case we want dataframe data
+        files = os.listdir(source_dir)
+        
+        indices = np.arange(len(files))
+        skf = KFold(n_splits=num_folds, shuffle=False)
+        for i, (train_indices, test_indices) in enumerate(skf.split(indices)):
+            if i == fold_id:
+                break
+        indices = train_indices if is_train else test_indices
+                
+        data = []
+        for i in indices:   
+            data.append(load_one_gz_file(os.path.join(source_dir, files[i]), field))
+        data = pd.concat(data, ignore_index=True)
+        data.reset_index(drop=True, inplace=True)
+        return data
+
+
+def collate_fn_tensor(batch: torch.tensor, target_treatment: list, target_task: list,):
+    # target_treatment: target treatment column idx
+    # target_task: target task column idx
+    features = torch.stack([sample[:536] for sample in batch])
+    assert min(min(target_treatment), min(target_task)) > 535, "check the indices for treatments or tasks"
+    treatments = torch.stack([sample[target_treatment] for sample in batch])
+    tasks = torch.stack([sample[target_task] for sample in batch])
+    return features, treatments, tasks
+
+
+def collate_fn_dict(batch, target_treatment, target_task):
+    assert min(min(target_treatment), min(target_task)) > 535, "check the indices for treatments or tasks"
+    treatments = torch.stack([sample[target_treatment] for sample in batch])
+    tasks = torch.stack([sample[target_task] for sample in batch])
+    features = torch.stack([list(sample.values())[:536] for sample in batch])
+    return features, treatments, tasks
+
+
+def collate_fn_ndarray(batch: np.ndarray, target_treatment: list, target_task: list):
+    features = [sample[:536] for sample in batch]
+    assert min(min(target_treatment), min(target_task)) > 535, "check the indices for treatments or tasks"
+    treatments = [sample[target_treatment] for sample in batch]
+    tasks = [sample[target_task] for sample in batch]
+    features = torch.tensor(features).to(torch.float32)
+    treatments = torch.tensor(treatments).to(torch.float32).squeeze()
+    tasks = torch.tensor(tasks).to(torch.float32).squeeze()
+    return features, treatments, tasks
 
 
 def create_folds(file_paths, n_folds=5):
@@ -30,6 +77,32 @@ def create_folds(file_paths, n_folds=5):
         test_files = [file for idx in test_index for file in sub_datasets[idx]]
         folds.append((train_files, test_files))
     return folds
+
+
+def get_data_(source_dir, target_treatment, target_task, fold_id, batch_size, num_folds=5):
+    # directly access gz files
+    # train_set = CustomDataset(source_dir, is_train=True, fold_id=fold_id, num_folds=num_folds)
+    # test_set  = CustomDataset(source_dir, is_train=False, fold_id=fold_id, num_folds=num_folds) 
+
+    # process parquet files. must use streaming or it may oom
+    # dataset = load_dataset('parquet', data_files='data/demo/dataset_demo.parquet', split='train', streaming=True)
+    # train_set = dataset.take(100000)
+    # test_set = dataset.skip(100000).take(20000)
+    
+    # split train and test
+    dataset = CustomDatasetHdf5('data/demo/dataset_demo.hdf5', None)
+    skf = KFold(n_splits=num_folds, shuffle=False)
+    for i, (train_indices, test_indices) in enumerate(skf.split(np.arange(len(dataset)))):
+        if i == fold_id:
+            break
+    train_set = CustomDatasetHdf5('data/demo/dataset_demo.hdf5', train_indices)
+    test_set  = CustomDatasetHdf5('data/demo/dataset_demo.hdf5', test_indices)
+    
+    collate_fn = partial(collate_fn_ndarray, target_treatment=target_treatment, target_task=target_task)
+    sampler = RandomSampler(train_set)
+    train_loader = DataLoader(train_set, sampler=sampler, batch_size=batch_size, collate_fn=collate_fn)
+    test_loader  = DataLoader(test_set, shuffle=False, batch_size=batch_size, collate_fn=collate_fn)
+    return train_loader, test_loader
 
 
 def collate_fn(batch, feature_index, treatment_index, task_index, group_discrete=False, pad=False):
@@ -50,10 +123,10 @@ def collate_fn(batch, feature_index, treatment_index, task_index, group_discrete
             # pad differnt groups features to the same length according to group_area
             grouped_disc_feature = [np.pad(f, ((0, 0), (0, 35 - f.shape[-1])), 'constant', constant_values=0)[:, None, :] for f in grouped_disc_feature]
             grouped_disc_feature = np.concatenate(grouped_disc_feature, axis=1)  # B 9 35
-            grouped_disc_feature = torch.as_tensor(grouped_disc_feature, dtype=torch.float32)
+            grouped_disc_feature = torch.as_tensor(grouped_disc_feature, dtype=torch.float)
         else:
             grouped_disc_feature = torch.as_tensor(batch[:, feature_groups.indices])
-        
+            
         return (features_tensor, grouped_disc_feature), treatments_tensor, tasks_tensor
 
     return features_tensor, treatments_tensor, tasks_tensor
@@ -71,20 +144,23 @@ class feature_groups:
         'group_district': list(range(722, 726)),
         'group_area': list(range(726, 761)),
         'group_r': list(range(761, 770)),
+        # 'group_rank5': list(range(121, 184)),
     }
     indices = list(range(685, 770))
+    # indices += list(range(121, 184))
 
-
-def get_data(train_files, test_files, target_treatment=None, target_task=None, batch_size=3840, dist=False, feature_group=None, addition_feat=None, pad=False):
+def get_data(train_files, test_files, target_treatment=None, target_task=None, batch_size=3840, dist=False, feature_group=None, addition_feat=None):
     with open('data/train_test_data/OUT_COLUMN_new', 'r') as f:
         labels = f.readlines()
     labels = [x.strip('\n') for x in labels]
         
     treatment_index = [idx for idx, elem in enumerate(labels) if elem == 'treatment_next_iswarm']
+    # treatment_next_is_9aiwarmround
     task_index = [idx for idx, elem in enumerate(labels) if elem == 'label_nextday_login']
     
     feature_index = [idx for idx, elem in enumerate(labels) if elem[:3] == 'fea']
     if feature_group != None:
+        pad = True if 'pad' in feature_group else False
         feature_index = list(filter(lambda i: i not in list(range(537, 607)) + feature_groups.indices, feature_index))
         collate = partial(collate_fn, feature_index=feature_index, treatment_index=treatment_index, task_index=task_index, group_discrete=True, pad=pad)
     else:
@@ -92,7 +168,7 @@ def get_data(train_files, test_files, target_treatment=None, target_task=None, b
         feature_index = list(filter(lambda i: i not in list(range(537, 607)), feature_index))  # filter out columns that are used for AI accompany
         collate = partial(collate_fn, feature_index=feature_index, treatment_index=treatment_index, task_index=task_index)
     
-    if addition_feat is not None:  # return additional features that can be used for further analysis
+    if addition_feat is not None:
         feature_index.extend(addition_feat)
     
     train_set = CustomDatasetHdf5MultiChunk(train_files, chunk_size=3840 * 16)
@@ -107,13 +183,37 @@ def get_data(train_files, test_files, target_treatment=None, target_task=None, b
     return train_loader, test_loader
 
 
-class FakeData(Dataset):
-    def __init__(self, x):
-        self.data = np.random.randn(10000, 685)
+
+def get_data_criteo(batch_size, fold_idx):
+    dataset = DatasetCriteo()
+    
+    skf = StratifiedKFold(n_splits=5)
+    indices = np.arange(len(dataset))
+    for i, (train_indices, test_indices) in enumerate(skf.split(indices, dataset.treat)):
+        if i == fold_idx:
+            train_set = Subset(dataset, train_indices)
+            test_set  = Subset(dataset, test_indices)
+
+            train_loader = DataLoader(train_set, shuffle=True, batch_size=batch_size, pin_memory=True, num_workers=16, drop_last=True)
+            test_loader  = DataLoader(test_set, shuffle=False, batch_size=batch_size, pin_memory=True, num_workers=16, drop_last=True)
+            return train_loader, test_loader
+
+
+class DatasetCriteo(Dataset):
+    def __init__(self):
+        self.data = np.load('data/tencent_data_zscore/criteo/criteo_data.npy').astype(np.float32)
+        self.treat = np.load('data/tencent_data_zscore/criteo/criteo_treat.npy', allow_pickle=True).astype(np.float32)
+        self.target = np.load('data/tencent_data_zscore/criteo/criteo_target.npy', allow_pickle=True).astype(np.float32)
+
+        self.data = torch.as_tensor(self.data)
+        self.treat =  torch.as_tensor(self.treat)
+        self.target = torch.as_tensor(self.target)
+    
     def __len__(self):
         return len(self.data)
+    
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.data[idx], self.treat[idx], self.target[idx]
    
 
 class CustomDatasetHdf5Multi(Dataset):
@@ -321,6 +421,70 @@ class CustomDataset(Dataset):
                 lengths[i] = sum(1 for line in f)
             total_length += lengths[i]
         return lengths, total_length
+
+
+class CustomDataLoader:
+    def __init__(self, dataset, batch_size=1, shuffle=False, num_workers=0, collate_fn=None):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.num_workers = num_workers
+        self.collate_fn = collate_fn if collate_fn else self.default_collate
+        self.queue = Queue(maxsize=num_workers * 2)
+        self.processes = []
+
+    def _worker(self, indices, queue):
+        worker_name = current_process().name
+        for idx in indices:
+            queue.put(self.dataset[idx])
+        queue.put(None)  # End-of-data signal
+
+    def __iter__(self):
+        indices = list(range(len(self.dataset)))
+        if self.shuffle:
+            np.random.shuffle(indices)
+
+        if self.num_workers > 0:
+            chunk_size = (len(indices) + self.num_workers - 1) // self.num_workers
+            for i in range(self.num_workers):
+                start_idx = i * chunk_size
+                end_idx = min((i + 1) * chunk_size, len(indices))
+                process = Process(target=self._worker, args=(indices[start_idx:end_idx], self.queue))
+                process.start()
+                self.processes.append(process)
+        else:
+            for idx in indices:
+                self.queue.put(self.dataset[idx])
+            self.queue.put(None)  # End-of-data signal
+
+        return self
+
+    def __next__(self):
+        batch = []
+        while len(batch) < self.batch_size:
+            item = self.queue.get()
+            if item is None:  # End-of-data signal
+                if len(batch) == 0:
+                    self._cleanup()
+                    raise StopIteration
+                else:
+                    break
+            batch.append(item)
+        return self.collate_fn(batch)
+
+    def __len__(self):
+        return (len(self.dataset) + self.batch_size - 1) // self.batch_size
+
+    def _cleanup(self):
+        for process in self.processes:
+            process.join()
+        self.processes = []
+
+    def __del__(self):
+        self._cleanup()
+
+    def default_collate(self, batch):
+        return torch.tensor(batch)
 
 
 def get_all_sizes(source_dir):
